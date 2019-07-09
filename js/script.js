1ea649ff44ae8e46d4dc8b9d2cdab7d5abc95774
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-07-09 20:27:30 +0100

    Update files

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ var output = document.getElementById('greeter-output');
 output.innerHTML = 'Click the button! I want to say hello!' + '<br><br>' + output.innerHTML;
 
 var button = document.getElementById('greeter-button');
-
 var userName;
 
 button.addEventListener('click', function(){
 	userName = window.prompt('What is your name?');
-	if userName == ''{
-  	output.innerHTML = 'Hello stranger' + '<br><br>' + output.innerHTML;
-  }
+	if (userName == '') {
+  	    output.innerHTML = 'Hello stranger' + '<br><br>' + output.innerHTML;
+    }
 
-  if else
-	output.innerHTML = 'Hello '+userName+'!' + '<br><br>' + output.innerHTML;
+    if (userName == null) {
+	   output.innerHTML = 'You have to enter your name!' + '<br><br>' + output.innerHTML;
+    }
 });
