133d22aae0976f55da3f7e715ac3c27713c21ee3
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-07-11 10:33:40 +0100

    Add next functionality to button

--- js/transfer-button.js
+++ js/transfer-button.js
@@ -6,15 +6,32 @@ output.innerHTML = 'Click the button to check temperature in Fahrenheit scale.' 
 var button = document.getElementById('greeter-button');
 var temperature;
 var Ftemperature;
+var regex = /^[0-9]+$/;
 
 button.addEventListener('click', function() {
     temperature = window.prompt('What\'s the temperature? Enter the number: ');
-    if (typeof temperature == 'number') {
-        output.innerHTML = 'You ddhave to ffenter the number!' + '<br><br>' + output.innerHTML;
+
+    if (!temperature.match(regex)) {
+        output.innerHTML = 'You have to enter the number!' + '<br><br>' + output.innerHTML;
     }
     else {
         Ftemperature = (temperature * 1.8) + 32;
         output.innerHTML = 'Celsius temperature is ' + temperature + '°C' + '\n' +
         ' and Fahrenheit temperature is ' + Ftemperature + '°F' + '<br><br>' + output.innerHTML;
+        if (temperature == 0) {
+            output.innerHTML = 'This temperature is a melting point of ice.' + '<br><br>' + output.innerHTML;
+        }
+        else if (Ftemperature > 120) {
+            output.innerHTML = 'Most bacteria do not live above this temperature.' + '<br><br>' + output.innerHTML;
+        }
+        else if (temperature >= 20) {
+            output.innerHTML = 'Summer is starting!' + '<br><br>' + output.innerHTML;
+        }
+        else if (temperature <= 10) {
+            output.innerHTML = 'We have winter right now.' + '<br><br>' + output.innerHTML;
+        }
+        else if (10 < temperature < 20) {
+            output.innerHTML = 'Winter is over, time for spring!.' + '<br><br>' + output.innerHTML;
+        }
     }
 });
