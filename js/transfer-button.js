d82b0b009799567ee9d2f8e553f052cc4687e8cd
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-07-14 19:24:07 +0100

    Finish task.

--- js/transfer-button.js
+++ js/transfer-button.js
@@ -8,34 +8,34 @@ foutput.innerHTML = 'Click the blue button to check temperature in Celcius scale
 
 var button = document.getElementById('greeter-button');
 var fbutton = document.getElementById('greeter-fbutton');
+
+var rbutton;
+
 var temperature;
 var fTemperature;
-var regex = /^[0-9]+$/;
 
 var zero = 0;
 var warm = 10;
 var hot = 20;
 var veryHot = 49;
 
-var answer
 var answer1 = 'This temperature is a melting point of ice.' + '<br><br>';
 var answer2 = 'Most bacteria do not live above this temperature.' + '<br><br>';
 var answer3 = 'Summer is starting!' + '<br><br>';
 var answer4 = 'We have winter right now.' + '<br><br>';
 var answer5 = 'Winter is over, time for spring!' + '<br><br>';
-var answer6 = 'I don\'t have info about this temperature.';
 
 var checkAnswer = function(number) {
     return number == zero ? answer1
-        : number > veryHot ? answer2
-        : number >= hot ? answer3
+        : (number < veryHot && number >= hot) ? answer3
         : number <= warm ? answer4
-        : warm < number < hot ? answer5
-        : answer6
+        : (warm < number && number < hot) ? answer5
+        : answer2;
 }
 
 var giveAnswer = function(text) {
-    return output.innerHTML = text + output.innerHTML;
+    return rbutton == true ? output.innerHTML = text + output.innerHTML
+        : foutput.innerHTML = text + foutput.innerHTML;
 }
 
 var cTempConvert = function(number) {
@@ -50,11 +50,11 @@ var fTempConvert = function(number) {
 
 button.addEventListener('click', function() {
     temperature = parseInt(window.prompt('What\'s the temperature? Enter the number: '));
-
     if (isNaN(temperature) == true) {
         output.innerHTML = 'You have to enter the number!' + '<br><br>';
     }
     else {
+        rbutton = true;
         fTemperature = cTempConvert(temperature);
         output.innerHTML = 'Celsius temperature is ' + temperature + '°C' + '\n' +
         ' and Fahrenheit temperature is ' + fTemperature + '°F' + '<br><br>';
@@ -63,31 +63,17 @@ button.addEventListener('click', function() {
     }
 });
 
-
 fbutton.addEventListener('click', function() {
-    fTemperature = window.prompt('What\'s the temperature? Enter the number: ');
-
-    if (!fTemperature.match(regex)) {
+    fTemperature = parseInt(window.prompt('What\'s the temperature? Enter the number: '));
+    if (isNaN(fTemperature) == true) {
         foutput.innerHTML = 'You have to enter the number!' + '<br><br>';
     }
     else {
+        rbutton = false;
         temperature = fTempConvert(fTemperature);
         foutput.innerHTML = 'Fahrenheit temperature is ' + fTemperature + '°F' + '\n' +
         ' and Celcius temperature is ' + temperature + '°C' + '<br><br>';
-        if (fTemperature == 32) {
-            foutput.innerHTML = 'This temperature is a melting point of ice.' + '<br><br>' + foutput.innerHTML;
-        }
-        else if (fTemperature > 120) {
-            foutput.innerHTML = 'Most bacteria do not live above this temperature.' + '<br><br>' + foutput.innerHTML;
-        }
-        else if (fTemperature >= 68) {
-            foutput.innerHTML = 'Summer is starting!' + '<br><br>' + foutput.innerHTML;
-        }
-        else if (fTemperature <= 50) {
-            foutput.innerHTML = 'We have winter right now.' + '<br><br>' + foutput.innerHTML;
-        }
-        else if (50 < fTemperature < 68) {
-            foutput.innerHTML = 'Winter is over, time for spring!.' + '<br><br>' + foutput.innerHTML;
-        }
+
+        giveAnswer(checkAnswer(temperature));
     }
 });
